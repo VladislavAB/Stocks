bfbd96b4bdf8822b8b3e53c7ee0f8a63aa60fc8a
VladislavAB <vlad.byrdin@bk.ru>
2023-12-22 17:24:32 +0700

    refact: added new methods

--- MEXC.py
+++ MEXC.py
@@ -14,6 +14,39 @@ class MEXC:
             self.symbols = load_form_json["data"]
         return self.symbols
 
+    def get_asks(self):
+        self.asks = {}
+        prices = []
+        url = 'https://api.mexc.com/api/v3/depth?symbol=WOFO1USDT'
+        mexc_api_respond = requests.get(url)
+        if mexc_api_respond:
+            load_form_json = json.loads(mexc_api_respond.text)
+            self.asks = load_form_json["asks"]
+            for i in self.asks:
+                prices.append(i[0])
+        return sorted(prices)
+
+    def get_bids(self):
+        self.bids = {}
+        prices = []
+        url = 'https://api.mexc.com/api/v3/depth?symbol=WOFO1USDT'
+        mexc_api_respond = requests.get(url)
+        if mexc_api_respond:
+            load_form_json = json.loads(mexc_api_respond.text)
+            self.bids = load_form_json["bids"]
+            for i in self.bids:
+                prices.append(i[0])
+        return sorted(prices, reverse=True)
+
+    def get_spread(self):
+        self.spread = {}
+        asks = self.get_asks()[0]
+        bids = self.get_bids()[0]
+        self.spread = {'ask': asks, 'bid': bids}
+        return self.spread
+
 
 pairs = MEXC()
-print(pairs.get_symbols())
+print(pairs.get_asks())
+print(pairs.get_bids())
+print(pairs.get_spread())
