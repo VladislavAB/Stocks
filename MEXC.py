bd9e3f49baf5c140214e96ad70c7aba08c910666
VladislavAB <vlad.byrdin@bk.ru>
2023-12-25 13:22:03 +0700

    refact: MEXC reading done

--- MEXC.py
+++ MEXC.py
@@ -4,49 +4,72 @@ import json
 
 class MEXC:
     def __init__(self):
-        self.symbols = []
+        self.base_url = 'https://api.mexc.com/api/v3/'
+        self.symbols = self.get_symbols()
 
     def get_symbols(self) -> list:
-        url = 'https://api.mexc.com/api/v3/defaultSymbols'
+        symbols = []
+        method = 'defaultSymbols'
+        url = self.base_url + method
         mexc_api_respond = requests.get(url)
         if mexc_api_respond:
             load_form_json = json.loads(mexc_api_respond.text)
-            self.symbols = load_form_json["data"]
-        return self.symbols
+            symbols = load_form_json["data"]
+        return symbols
 
-    def get_asks(self):
-        self.asks = {}
-        prices = []
-        url = 'https://api.mexc.com/api/v3/depth?symbol=WOFO1USDT'
+    def check_exist(self, pair: str) -> bool:
+        if pair in self.symbols:
+            return True
+        else:
+            print(f'Символа {pair} не сущеуствует!')
+            return False
+
+    def get_asks(self, pair: str) -> list or bool:
+        if self.check_exist(pair):
+            asks = []
+            method = "depth"
+            url = self.base_url + method + "?symbol=" + pair
+            mexc_api_respond = requests.get(url)
+            if mexc_api_respond:
+                load_form_json = json.loads(mexc_api_respond.text)
+                asks = load_form_json["asks"]
+            return asks
+        return False
+
+    def get_bids(self, pair: str) -> list or bool:
+        if self.check_exist(pair):
+            bids = []
+            method = "depth"
+            url = self.base_url + method + "?symbol=" + pair
+            mexc_api_respond = requests.get(url)
+            if mexc_api_respond:
+                load_form_json = json.loads(mexc_api_respond.text)
+                bids = load_form_json["bids"]
+            return bids
+        return False
+
+    def get_spread(self, pair: str) -> dict:
+        asks = self.get_asks(pair)
+        bids = self.get_bids(pair)
+        spread = {'ask': asks[0], 'bid': bids[0]}
+        return spread
+
+    def get_commission(self, pair: str) -> dict:
+        commission = {}
+        method = "exchangeInfo"
+        url = self.base_url + method + "?symbol=" + pair
         mexc_api_respond = requests.get(url)
         if mexc_api_respond:
             load_form_json = json.loads(mexc_api_respond.text)
-            self.asks = load_form_json["asks"]
-            for i in self.asks:
-                prices.append(i[0])
-        return sorted(prices)
-
-    def get_bids(self):
-        self.bids = {}
-        prices = []
-        url = 'https://api.mexc.com/api/v3/depth?symbol=WOFO1USDT'
-        mexc_api_respond = requests.get(url)
-        if mexc_api_respond:
-            load_form_json = json.loads(mexc_api_respond.text)
-            self.bids = load_form_json["bids"]
-            for i in self.bids:
-                prices.append(i[0])
-        return sorted(prices, reverse=True)
-
-    def get_spread(self):
-        self.spread = {}
-        asks = self.get_asks()[0]
-        bids = self.get_bids()[0]
-        self.spread = {'ask': asks, 'bid': bids}
-        return self.spread
+            maker_commission = load_form_json["symbols"][0]["makerCommission"]
+            taker_commission = load_form_json["symbols"][0]["takerCommission"]
+            commission = {'maker': maker_commission, 'taker': taker_commission}
+        return commission
 
 
-pairs = MEXC()
-print(pairs.get_asks())
-print(pairs.get_bids())
-print(pairs.get_spread())
+stock = MEXC()
+print(stock.get_symbols())
+print(stock.get_asks("WOFO1USDT"))
+print(stock.get_bids("WOFO1USDT"))
+print(stock.get_spread("WOFO1USDT"))
+print(stock.get_commission("WOFO1USDT"))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Stocks
 project about stock trading
+# API Docs:
+MEXC: https://mexcdevelop.github.io/apidocs/spot_v3_en/#exchange-information
